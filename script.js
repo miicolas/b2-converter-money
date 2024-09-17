26fe705df3b098549952b2e1a5fe52338d5719f7
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-09-17 13:31:10 +0200

    Add conversion rate display and update UI elements

--- script.js
+++ script.js
@@ -1,182 +1,39 @@
 const API_KEY = "9d74faa4a852a3bfa0022476";
 
-const CURRENCIES = [
-  "AED",
-  "AFN",
-  "ALL",
-  "AMD",
-  "ANG",
-  "AOA",
-  "ARS",
-  "AUD",
-  "AWG",
-  "AZN",
-  "BAM",
-  "BBD",
-  "BDT",
-  "BGN",
-  "BHD",
-  "BIF",
-  "BMD",
-  "BND",
-  "BOB",
-  "BRL",
-  "BSD",
-  "BTN",
-  "BWP",
-  "BYN",
-  "BZD",
-  "CAD",
-  "CDF",
-  "CHF",
-  "CLP",
-  "CNY",
-  "COP",
-  "CRC",
-  "CUP",
-  "CVE",
-  "CZK",
-  "DJF",
-  "DKK",
-  "DOP",
-  "DZD",
-  "EGP",
-  "ERN",
-  "ETB",
-  "EUR",
-  "FJD",
-  "FKP",
-  "FOK",
-  "GBP",
-  "GEL",
-  "GGP",
-  "GHS",
-  "GIP",
-  "GMD",
-  "GNF",
-  "GTQ",
-  "GYD",
-  "HKD",
-  "HNL",
-  "HRK",
-  "HTG",
-  "HUF",
-  "IDR",
-  "ILS",
-  "IMP",
-  "INR",
-  "IQD",
-  "IRR",
-  "ISK",
-  "JEP",
-  "JMD",
-  "JOD",
-  "JPY",
-  "KES",
-  "KGS",
-  "KHR",
-  "KID",
-  "KMF",
-  "KRW",
-  "KWD",
-  "KYD",
-  "KZT",
-  "LAK",
-  "LBP",
-  "LKR",
-  "LRD",
-  "LSL",
-  "LYD",
-  "MAD",
-  "MDL",
-  "MGA",
-  "MKD",
-  "MMK",
-  "MNT",
-  "MOP",
-  "MRU",
-  "MUR",
-  "MVR",
-  "MWK",
-  "MXN",
-  "MYR",
-  "MZN",
-  "NAD",
-  "NGN",
-  "NIO",
-  "NOK",
-  "NPR",
-  "NZD",
-  "OMR",
-  "PAB",
-  "PEN",
-  "PGK",
-  "PHP",
-  "PKR",
-  "PLN",
-  "PYG",
-  "QAR",
-  "RON",
-  "RSD",
-  "RUB",
-  "RWF",
-  "SAR",
-  "SBD",
-  "SCR",
-  "SDG",
-  "SEK",
-  "SGD",
-  "SHP",
-  "SLE",
-  "SOS",
-  "SRD",
-  "SSP",
-  "STN",
-  "SYP",
-  "SZL",
-  "THB",
-  "TJS",
-  "TMT",
-  "TND",
-  "TOP",
-  "TRY",
-  "TTD",
-  "TVD",
-  "TWD",
-  "TZS",
-  "UAH",
-  "UGX",
-  "USD",
-  "UYU",
-  "UZS",
-  "VES",
-  "VND",
-  "VUV",
-  "WST",
-  "XAF",
-  "XCD",
-  "XDR",
-  "XOF",
-  "XPF",
-  "YER",
-  "ZAR",
-  "ZMW",
-  "ZWL",
+const CURRENCIES = [ // Liste des devises disponibles
+  "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", 
+  "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL",
+  "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY",
+  "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP",
+  "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS",
+  "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
+  "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD",
+  "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT",
+  "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD",
+  "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
+  "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK",
+  "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR",
+  "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SOS", "SRD", "SSP",
+  "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD",
+  "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND",
+  "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW",
+  "ZWL"
 ];
 
-const fromCurrency = document.getElementById("from");
-const toCurrency = document.getElementById("to");
+const fromCurrency = document.getElementById("from"); // Récupération de l'input avec la devise de départ
+const toCurrency = document.getElementById("to"); // Récupération de l'input avec la devise souhaitée
 
-CURRENCIES.forEach((currency) => {
-  const option = document.createElement("option");
+CURRENCIES.forEach((currency) => { // Ajout des options dans les inputs
+  const option = document.createElement("option"); 
   option.value = currency;
   option.innerText = currency;
-  fromCurrency.appendChild(option);
-  toCurrency.appendChild(option.cloneNode(true));
+  fromCurrency.appendChild(option); 
+  toCurrency.appendChild(option.cloneNode(true)); 
 });
 
 const amount = document.getElementById("amount"); // Récupération de l'input avec le montant
 const result = document.getElementById("result"); // Récupération de l'input avec la conversion du montant
+const conversionRateInput = document.getElementById("conversionRate"); // Récupération de l'input avec le taux de conversion
 
 amount.addEventListener("input", async (event) => {
   // Ajout d'un listener sur l'input pour la conversion
@@ -187,6 +44,7 @@ amount.addEventListener("input", async (event) => {
   if (value === "" || value === null || isNaN(value) || value <= 0) {
     // Si le montant est invalide
     result.innerText = "0.00";
+    conversionRateInput.innerText = "0.00"; // Réinitialise le taux de conversion affiché
     return;
   }
 
@@ -201,15 +59,18 @@ amount.addEventListener("input", async (event) => {
         },
       }
     );
-    if (!response.ok)
-      // Si l'erreur est renvoyée par l'API
-      throw new Error(`HTTP erreur! statut: ${response.status}`);
+    if (!response.ok) throw new Error(`HTTP erreur! statut: ${response.status}`);
     const data = await response.json(); // Récupération de la réponse
     if (data.result === "error") throw new Error(data["error-type"]);
+    
+    const conversionRate = data.conversion_rate; // Récupération du taux de conversion
+    conversionRateInput.innerText = conversionRate.toFixed(2); // Affichage du taux de conversion arrondie à 2 décimales
+
     const convertedAmount = data.conversion_result; // Récupération de la conversion
     result.innerText = convertedAmount.toFixed(2); // Affichage de la conversion arrondie à 2 décimales
   } catch (error) {
     console.log(error);
     result.innerText = "0.00";
+    conversionRateInput.innerText = "0.00"; // Afficher 0.00 si une erreur se produit
   }
 });
